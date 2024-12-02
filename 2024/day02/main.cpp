753106e5b1282d5dae3e4e6e945febc0dd619d1b
Daniel Eisele <daniel_eisele@gmx.de>
2024-12-02 18:54:59 +0100

    Add solution for day 2

--- 2024/day02/main.cpp
+++ 2024/day02/main.cpp
@@ -4,16 +4,10 @@
 #include <QStringList>
 #include <QList>
 #include <QDebug>
-#include <csignal>
 
-void handler(int signal) {
-    Q_UNUSED(signal);
-    Q_ASSERT(0);
-}
+bool isSafe(const QList<int> &report);
 
 int main(int argc, char *argv[]) {
-    signal(SIGABRT, &handler);
-
     QTextStream out(stdout);
 
     QFile file("input");
@@ -24,9 +18,58 @@ int main(int argc, char *argv[]) {
 
     QTextStream in(&file);
 
+    QList<QList<int>> reports;
     while (!in.atEnd()) {
         QString line = in.readLine();
+        QStringList parts = line.split(" ");
+
+        QList<int> report;
+        for (const QString &part: parts) {
+            report.append(part.toInt());
+        }
+
+        reports.append(report);
     }
 
+    int safeReportsPart1 = 0;
+    for (const QList<int> &report: reports) {
+        if (isSafe(report)) {
+            safeReportsPart1++;
+        }
+    }
+
+    out << "Safe reports part 1: " << safeReportsPart1 << "\n";
+
+    int safeReportsPart2 = 0;
+    for (const QList<int> &report: reports) {
+        // if the original report is safe, the modified report will also be safe, so we don't need to check it
+        for (int i = 0; i < report.size(); i++) {
+            QList<int> modifiedReport = report;
+            modifiedReport.removeAt(i);
+
+            if (isSafe(modifiedReport)) {
+                safeReportsPart2++;
+                break;
+            }
+        }
+    }
+
+    out << "Safe reports part 2: " << safeReportsPart2 << "\n";
+
     return 0;
+}
+
+bool isSafe(const QList<int> &report) {
+    for (int i = 0; i < report.size() - 1; i++) {
+        int diff = report[i + 1] - report[i];
+        if (report[1] - report[0] < 0) {
+            diff *= -1;
+        }
+
+        if (diff < 1 || diff > 3) {
+            return false;
+        }
+    }
+
+    return true;
 }
