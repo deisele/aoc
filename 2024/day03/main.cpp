9efcc7d5f61cef543d461254f03061bee6e84569
Daniel Eisele <daniel_eisele@gmx.de>
2024-12-03 16:51:41 +0100

    Add solution for day 3

--- 2024/day03/main.cpp
+++ 2024/day03/main.cpp
@@ -1,32 +1,36 @@
 #include <QFile>
-#include <QTextStream>
 #include <QString>
-#include <QStringList>
-#include <QList>
-#include <QDebug>
-#include <csignal>
+#include <QRegularExpression>
+#include <iostream>
 
-void handler(int signal) {
-    Q_UNUSED(signal);
-    Q_ASSERT(0);
-}
-
-int main(int argc, char *argv[]) {
-    signal(SIGABRT, &handler);
-
-    QTextStream out(stdout);
-
+int main() {
     QFile file("input");
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qFatal() << "Cannot open input";
+        std::cerr << "Cannot open input";
         return 1;
     }
 
-    QTextStream in(&file);
+    QString input = file.readAll();
 
-    while (!in.atEnd()) {
-        QString line = in.readLine();
+    QRegularExpression command(R"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))");
+
+    bool enabled = true;
+    int sumPart1 = 0;
+    int sumPart2 = 0;
+    for (QRegularExpressionMatch match: command.globalMatch(input)) {
+        if (match.captured().startsWith("mul")) {
+            int product = match.captured(1).toInt() * match.captured(2).toInt();
+            sumPart1 += product;
+            if (enabled) {
+                sumPart2 += product;
+            }
+        } else {
+            enabled = (match.captured() == "do()");
+        }
     }
+
+    std::cout << "Sum part 1: " << sumPart1 << "\n";
+    std::cout << "Sum part 2: " << sumPart2 << "\n";
 
     return 0;
 }
