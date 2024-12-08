383d17a4c3955ef646e0e6f8c50c316945969c13
Daniel Eisele <daniel_eisele@gmx.de>
2024-12-08 23:50:10 +0100

    Add solution for day 5

--- 2024/day05/main.cpp
+++ 2024/day05/main.cpp
@@ -3,17 +3,15 @@
 #include <QString>
 #include <QStringList>
 #include <QList>
+#include <QSet>
+#include <QMap>
+#include <QMultiMap>
+#include <QQueue>
 #include <QDebug>
-#include <csignal>
 
-void handler(int signal) {
-    Q_UNUSED(signal);
-    Q_ASSERT(0);
-}
+QList<int> topologicalSort(const QMultiMap<int, int> &edges, const QList<int> &nodes);
 
 int main() {
-    signal(SIGABRT, &handler);
-
     QTextStream out(stdout);
 
     QFile file("input");
@@ -24,9 +22,77 @@ int main() {
 
     QTextStream in(&file);
 
+    QMultiMap<int, int> edges;
     while (!in.atEnd()) {
         QString line = in.readLine();
+        if (line.isEmpty()) {
+            break;
+        }
+
+        QStringList parts = line.split("|");
+        edges.insert(parts[0].toInt(), parts[1].toInt());
     }
 
+    int sumPart1 = 0;
+    int sumPart2 = 0;
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        QStringList parts = line.split(",");
+
+        QList<int> row;
+        for (const QString &part: parts) {
+            row.append(part.toInt());
+        }
+
+        QList<int> sorted = topologicalSort(edges, row);
+        if (row == sorted) {
+            sumPart1 += row[row.size() / 2];
+        } else {
+            sumPart2 += sorted[sorted.size() / 2];
+        }
+    }
+
+    out << "Sum part 1: " << sumPart1 << "\n";
+    out << "Sum part 2: " << sumPart2 << "\n";
+
     return 0;
+}
+
+QList<int> topologicalSort(const QMultiMap<int, int> &edges, const QList<int> &nodes) {
+    QSet<int> nodeSet(nodes.begin(), nodes.end());
+    QMap<int, int> inDegree;
+    for (int node: nodeSet) {
+        for (int to: edges.values(node)) {
+            if (nodeSet.contains(to)) {
+                inDegree[to]++;
+            }
+        }
+    }
+
+    QQueue<int> next;
+    for (int node: nodeSet) {
+        if (inDegree[node] == 0) {
+            next.enqueue(node);
+        }
+    }
+
+    QList<int> sorted;
+    while (!next.empty()) {
+        int node = next.dequeue();
+        sorted.append(node);
+
+        int enqueued = 0;
+        for (int to: edges.values(node)) {
+            inDegree[to]--;
+            if (inDegree[to] == 0) {
+                if (enqueued++ > 0) {
+                    // for the puzzle input, the sorting seems to be unique, so this should not happen
+                    qFatal() << "Multiple nodes with in-degree 0";
+                }
+                next.enqueue(to);
+            }
+        }
+    }
+
+    return sorted;
 }
