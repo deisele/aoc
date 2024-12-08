54130ca6cda11fa937595b3e74a0454e791e94fc
Daniel Eisele <daniel_eisele@gmx.de>
2024-12-08 22:09:46 +0100

    Add solution for day 4

--- 2024/day04/main.cpp
+++ 2024/day04/main.cpp
@@ -3,17 +3,10 @@
 #include <QString>
 #include <QStringList>
 #include <QList>
+#include <QPoint>
 #include <QDebug>
-#include <csignal>
 
-void handler(int signal) {
-    Q_UNUSED(signal);
-    Q_ASSERT(0);
-}
-
-int main(int argc, char *argv[]) {
-    signal(SIGABRT, &handler);
-
+int main() {
     QTextStream out(stdout);
 
     QFile file("input");
@@ -24,9 +17,50 @@ int main(int argc, char *argv[]) {
 
     QTextStream in(&file);
 
+    QList<QString> lines;
     while (!in.atEnd()) {
-        QString line = in.readLine();
+        lines.append(in.readLine());
     }
+
+    QList<QPoint> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+    int sizeX = lines.first().size();
+    int sizeY = lines.size();
+
+    int sumPart1 = 0;
+    for (int y = 0; y < sizeY; y++) {
+        for (int x = 0; x < sizeX; x++) {
+            for (const QPoint &direction: directions) {
+                const char pattern[] = "XMAS";
+
+                QPoint pos = {x, y};
+                int i = 0;
+                while (pos.x() >= 0 && pos.x() < sizeX && pos.y() >= 0 && pos.y() < sizeY && lines[pos.y()][pos.x()] == pattern[i]) {
+                    pos += direction;
+                    i++;
+                }
+
+                if (i == sizeof(pattern) - 1) {
+                    sumPart1++;
+                }
+            }
+        }
+    }
+
+    out << "Sum part 1: " << sumPart1 << "\n";
+
+    int sumPart2 = 0;
+    for (int y = 1; y < sizeY - 1; y++) {
+        for (int x = 1; x < sizeX - 1; x++) {
+            if (lines[y][x] == 'A' &&
+                ((lines[y - 1][x - 1] == 'M' && lines[y + 1][x + 1] == 'S') || (lines[y - 1][x - 1] == 'S' && lines[y + 1][x + 1] == 'M')) &&
+                ((lines[y - 1][x + 1] == 'M' && lines[y + 1][x - 1] == 'S') || (lines[y - 1][x + 1] == 'S' && lines[y + 1][x - 1] == 'M'))) {
+
+                sumPart2++;
+            }
+        }
+    }
+
+    out << "Sum part 2: " << sumPart2 << "\n";
 
     return 0;
 }
