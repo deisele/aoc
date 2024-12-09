e6055f04ea6e10b71bdd8f871975b2c927cb4b2f
Daniel Eisele <daniel_eisele@gmx.de>
2024-12-09 20:20:32 +0100

    Add solution for day 9

--- 2024/day09/main.cpp
+++ 2024/day09/main.cpp
@@ -4,16 +4,10 @@
 #include <QStringList>
 #include <QList>
 #include <QDebug>
-#include <csignal>
 
-void handler(int signal) {
-    Q_UNUSED(signal);
-    Q_ASSERT(0);
-}
+long long checksum(const QList<int> &blocks);
 
 int main() {
-    signal(SIGABRT, &handler);
-
     QTextStream out(stdout);
 
     QFile file("input");
@@ -23,10 +17,73 @@ int main() {
     }
 
     QTextStream in(&file);
+    QString diskMap = in.readLine();
 
-    while (!in.atEnd()) {
-        QString line = in.readLine();
+    QList<int> blocks;
+    int id = 0;
+    bool isData = true;
+    for (QChar block: diskMap) {
+        blocks.append(QList<int>(block.digitValue(), (isData? id++ : -1)));
+        isData = !isData;
     }
 
+    QList<int> compactedBlocks1 = blocks;
+    for (int i = 0; i < compactedBlocks1.size(); i++) {
+        if (compactedBlocks1[i] == -1) {
+            compactedBlocks1[i] = compactedBlocks1.takeLast();
+        }
+
+        while (compactedBlocks1.last() == -1) {
+            compactedBlocks1.removeLast();
+        }
+    }
+
+    out << "Sum part 1: " << checksum(compactedBlocks1) << "\n";
+
+    QList<int> compactedBlocks2 = blocks;
+    int top = compactedBlocks2.size() - 1;
+    while (top > 0) {
+        while (compactedBlocks2[top] == -1) {
+            top--;
+        }
+
+        int dst = top;
+        int id = compactedBlocks2[top];
+        int size = 0;
+        while (top >= 0 && compactedBlocks2[top] == id) {
+            compactedBlocks2[top--] = -1;
+            size++;
+        }
+
+        int freeSpace = 0;
+        for (int i = 0; i <= top; i++) {
+            if (compactedBlocks2[i] == -1) {
+                if (++freeSpace == size) {
+                    dst = i;
+                    break;
+                }
+            } else {
+                freeSpace = 0;
+            }
+        }
+
+        while (size --> 0) {
+            compactedBlocks2[dst--] = id;
+        }
+    }
+
+    out << "Sum part 2: " << checksum(compactedBlocks2) << "\n";
+
     return 0;
+}
+
+long long checksum(const QList<int> &blocks) {
+    long long sum = 0;
+    for (int i = 0; i < blocks.size(); i++) {
+        if (blocks[i] != -1) {
+            sum += i * blocks[i];
+        }
+    }
+
+    return sum;
 }
