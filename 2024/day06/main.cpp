e9bf0efc398ac2ea634e18dae92a492904abc55b
Daniel Eisele <daniel_eisele@gmx.de>
2024-12-09 01:02:52 +0100

    Add solution for day 6

--- 2024/day06/main.cpp
+++ 2024/day06/main.cpp
@@ -3,17 +3,14 @@
 #include <QString>
 #include <QStringList>
 #include <QList>
+#include <QSet>
+#include <QPair>
+#include <QPoint>
 #include <QDebug>
-#include <csignal>
 
-void handler(int signal) {
-    Q_UNUSED(signal);
-    Q_ASSERT(0);
-}
+int cellsVisitedUntilOutOfBounds(const QStringList &grid, QPoint start);
 
 int main() {
-    signal(SIGABRT, &handler);
-
     QTextStream out(stdout);
 
     QFile file("input");
@@ -24,9 +21,67 @@ int main() {
 
     QTextStream in(&file);
 
+    QStringList grid;
     while (!in.atEnd()) {
-        QString line = in.readLine();
+        grid.append(in.readLine());
     }
 
+    QPoint start;
+    for (int y = 0; y < grid.size(); y++) {
+        for (int x = 0; x < grid[y].size(); x++) {
+            if (grid[y][x] == '^') {
+                start = {x, y};
+                goto found;
+            }
+        }
+    }
+    found:
+
+    out << "Part 1: " << cellsVisitedUntilOutOfBounds(grid, start) << "\n";
+
+    int sumPart2 = 0;
+    for (int y = 0; y < grid.size(); y++) {
+        for (int x = 0; x < grid[y].size(); x++) {
+            if (grid[y][x] == '.') {
+                grid[y][x] = '#';
+
+                if (cellsVisitedUntilOutOfBounds(grid, start) == -1) {
+                    sumPart2++;
+                }
+
+                grid[y][x] = '.';
+            }
+        }
+    }
+
+    out << "Part 2: " << sumPart2 << "\n";
+
     return 0;
+}
+
+int cellsVisitedUntilOutOfBounds(const QStringList &grid, QPoint start) {
+    QList<QPoint> directions = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
+
+    QSet<QPoint> visited;
+    QSet<QPair<QPoint, int>> visitedWithDirection;
+    QPoint pos = start;
+    int direction = 0;
+    while (0 <= pos.x() && pos.x() < grid[0].size() && 0 <= pos.y() && pos.y() < grid.size()) {
+        if (visitedWithDirection.contains({pos, direction})) {
+            return -1;
+        }
+
+        visited.insert(pos);
+        visitedWithDirection.insert({pos, direction});
+
+        QPoint next = pos + directions[direction];
+        while (0 <= next.x() && next.x() < grid[0].size() && 0 <= next.y() && next.y() < grid.size() && grid[next.y()][next.x()] == '#') {
+            direction = (direction + 1) % directions.size();
+            next = pos + directions[direction];
+        }
+
+        pos = next;
+    }
+
+    return visited.size();
 }
