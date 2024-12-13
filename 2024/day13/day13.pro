47f4e75123e0cba55dfa5067dd526bb1c6d58dc6
Daniel Eisele <daniel_eisele@gmx.de>
2024-12-13 18:14:45 +0100

    Add solution for day 13

--- 2024/day13/day13.pro
+++ 2024/day13/day13.pro
@@ -1,7 +1,6 @@
 QT = core
 
 CONFIG += cmdline c++20
-QMAKE_CXXFLAGS += -ftrapv
 
 SOURCES += \
         main.cpp
--- 2024/day13/main.cpp
+++ 2024/day13/main.cpp
@@ -4,16 +4,25 @@
 #include <QStringList>
 #include <QList>
 #include <QDebug>
-#include <csignal>
+#include <compare>
 
-void handler(int signal) {
-    Q_UNUSED(signal);
-    Q_ASSERT(0);
-}
+struct Vec2 {
+    long long x, y;
+
+    std::strong_ordering operator<=>(const Vec2 &b) const = default;
+    Vec2 operator+(const Vec2 &b) const { return {x + b.x, y + b.y}; }
+    Vec2 operator*(long long s) const { return {x * s, y * s}; }
+};
+
+struct Machine {
+    Vec2 a, b;
+
+    Vec2 prize;
+};
+
+long long solve(const QList<Machine> &machines, Vec2 offset = {0, 0});
 
 int main() {
-    signal(SIGABRT, &handler);
-
     QTextStream out(stdout);
 
     QFile file("input");
@@ -24,9 +33,40 @@ int main() {
 
     QTextStream in(&file);
 
+    QList<Machine> machines;
     while (!in.atEnd()) {
-        QString line = in.readLine();
+        auto readVec2 = [&in] () {
+            QStringList parts = in.readLine().split(": ")[1].split(", ");
+            return Vec2(parts[0].sliced(2).toInt(), parts[1].sliced(2).toInt());
+        };
+
+        Machine machine;
+        machine.a = readVec2();
+        machine.b = readVec2();
+        machine.prize = readVec2();
+        in.readLine(); // empty line
+
+        machines.append(machine);
     }
 
+    out << "Part 1: " << solve(machines) << "\n";
+    long long offset = 10'000'000'000'000;
+    out << "Part 2: " << solve(machines, {offset, offset}) << "\n";
+
     return 0;
+}
+
+long long solve(const QList<Machine> &machines, Vec2 offset) {
+    long long sum = 0;
+    for (const Machine &machine: machines) {
+        Vec2 offsetPrice = machine.prize + offset;
+        long long aPresses = (offsetPrice.x * machine.b.y - offsetPrice.y * machine.b.x) / (machine.a.x * machine.b.y - machine.a.y * machine.b.x);
+        long long bPresses = (machine.a.x * offsetPrice.y - machine.a.y * offsetPrice.x) / (machine.a.x * machine.b.y - machine.a.y * machine.b.x);
+
+        if (machine.a * aPresses + machine.b * bPresses == offsetPrice) {
+            sum += 3 * aPresses + bPresses;
+        }
+    }
+
+    return sum;
 }
